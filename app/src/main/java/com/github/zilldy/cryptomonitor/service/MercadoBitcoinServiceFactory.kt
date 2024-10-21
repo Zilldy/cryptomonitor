21f64718bc17ecae65ceeddc67e4d38868340ebb
zilldy <guicardosobarreiro1@gmail.com>
2024-10-21 12:07:15 -0300

    .

--- app/src/main/java/com/github/zilldy/cryptomonitor/service/MercadoBitcoinServiceFactory.kt
+++ app/src/main/java/com/github/zilldy/cryptomonitor/service/MercadoBitcoinServiceFactory.kt
@@ -4,6 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MercadoBitcoinServiceFactory {
+
     fun create(): MercadoBitcoinService {
         val retrofit = Retrofit.Builder()
             .baseUrl("https://www.mercadobitcoin.net/")
