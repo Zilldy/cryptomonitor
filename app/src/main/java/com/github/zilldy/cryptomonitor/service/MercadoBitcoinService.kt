7a7e2f648b07c266b2caca785d18346f58600b60
zilldy <guicardosobarreiro1@gmail.com>
2024-10-21 11:12:27 -0300

    Corrigindo erro de importação

--- app/src/main/java/com/github/zilldy/cryptomonitor/service/MercadoBitcoinService.kt
+++ app/src/main/java/com/github/zilldy/cryptomonitor/service/MercadoBitcoinService.kt
@@ -1,7 +1,7 @@
 package com.github.zilldy.cryptomonitor.service
 
 import com.github.zilldy.cryptomonitor.model.TickerResponse
-import okhttp3.Response
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface MercadoBitcoinService {
